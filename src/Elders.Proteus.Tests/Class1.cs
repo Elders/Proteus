a32a5de5ce148d86523efac632ac2d8e991cf5a5
simeon.dimov <simeon.dimov@mentormate.com>
2015-01-12 18:04:37 +0200

    Fixed nested types serialization

---
+++ src/Elders.Proteus.Tests/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+
+namespace Elders.Proteus.Tests
+{
+    [Subject(typeof(Serializer))]
+    public class Whem_stackoverflow
+    {
+
+        Establish context = () =>
+        {
+            ser = new C() { CString = "C", Aprop = new A() { AString = "A" } };
+
+            serializer = new Serializer();
+            serializer2 = new Serializer();
+            serStream = new MemoryStream();
+            serializer.SerializeWithHeaders(serStream, ser);
+            serStream.Position = 0;
+        };
+        Because of_deserialization = () => { deser = (C)serializer.DeserializeWithHeaders(serStream); };
+
+        It should_not_be_null = () => deser.ShouldNotBeNull();
+
+
+        static Exception ex;
+        static C ser;
+        static C deser;
+        static Stream serStream;
+        static Serializer serializer;
+        static Serializer serializer2;
+    }
+
+    [DataContract(Name = "79bb4bef-13d0-4a24-8e5a-f52e38b73eff")]
+    public class A
+    {
+        [DataMember(Order = 1)]
+        public string AString { get; set; }
+    }
+    [DataContract(Name = "70eb3522-0670-4449-a514-cd0082cededb")]
+    public class B : A
+    {
+        [DataMember(Order = 2)]
+        public string BString { get; set; }
+    }
+    [DataContract(Name = "78eb1d0c-9284-4f17-b8e5-9a29e5cc7a5b")]
+    public class C
+    {
+        [DataMember(Order = 3)]
+        public string CString { get; set; }
+
+        [DataMember(Order = 4)]
+        public A Aprop { get; set; }
+    }
+}
---
+++ src/Elders.Proteus/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf.Meta;
+
+namespace Elders.Proteus
+{
+    public static class ModelBuilder
+    {
+
+        public static RuntimeTypeModel New()
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture; // use InvariantCulture or other if you prefer
+            var instance = (RuntimeTypeModel)
+              Activator.CreateInstance(typeof(RuntimeTypeModel), BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] { true }, culture);
+            instance.AutoAddMissingTypes = true;
+            return instance;
+        }
+
+        public static void IncludeParents(this RuntimeTypeModel model, ITypeIdentifier typeIdentifier, MetaType type)
+        {
+            if (type.Type.BaseType == typeof(object) || type.Type.BaseType == null)
+                return;
+            var metaBase = model.Add(type.Type.BaseType, true);
+            model.IncludeParents(typeIdentifier, metaBase);
+            if (typeIdentifier.AvailableTypes.Contains(type.Type.BaseType))
+                foreach (var field in metaBase.GetFields())
+                {
+                    if (!type.GetFields().Select(x => x.FieldNumber).Contains(field.FieldNumber))
+                        type.AddField(field.FieldNumber, field.Member.Name);
+                }
+        }
+
+    }
+}
